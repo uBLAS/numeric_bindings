00419b4ff3bdc92e1d785c91976342f199142a94
rutger <rutger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-09 12:51:22 +0000

    fixed nlvl computation in gelsd
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@58832 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/numeric/bindings/tools/templates/driver/gelsd.hpp
+++ libs/numeric/bindings/tools/templates/driver/gelsd.hpp
@@ -9,7 +9,7 @@ $INTEGER_TYPE minmn = std::min< $INTEGER_TYPE >( size_row(a), size_column(a) );
 $TEMPLATE[gelsd.all.SMLSIZ.init]
 $INTEGER_TYPE smlsiz = ilaenv(9, "GELSD", "");
 $TEMPLATE[gelsd.all.NLVL.init]
-$INTEGER_TYPE nlvl = static_cast<$INTEGER_TYPE>(((std::log(static_cast<real_type>(minmn)) / std::log(static_cast<real_type>(2.))) / (smlsiz+1)) + 1);
+$INTEGER_TYPE nlvl = static_cast<$INTEGER_TYPE>(std::log(static_cast<real_type>(minmn)/static_cast<real_type>(smlsiz+1))/std::log(2.0)) + 1;
 $TEMPLATE[gelsd.complex.min_size_rwork]
 $INTEGER_TYPE smlsiz_plus_one = smlsiz + 1;
 return std::max< $INTEGER_TYPE >( 1, 10*minmn + 2*minmn*smlsiz + 8*minmn*nlvl + 3*smlsiz*nrhs + smlsiz_plus_one * smlsiz_plus_one );

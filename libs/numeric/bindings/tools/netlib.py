2d58e23a259c841713289e1b7de612f6b4e2b511
rutger <rutger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-12 13:08:25 +0000

    support for detection of bandwidth of upper/lower parts selected by uplo tag
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@58937 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/numeric/bindings/tools/netlib.py
+++ libs/numeric/bindings/tools/netlib.py
@@ -216,6 +216,10 @@ def call_level0_type( name, properties, arg_map ):
         result = "bandwidth_upper_op(" + properties[ 'trait_of' ][ 0 ].lower() + \
             ", " + arg_map[ properties[ 'trait_of' ][ 0 ] ][ 'ref_trans' ].lower() + "())"
 
+    if properties[ 'trait_type' ] == 'num_super_uplo':
+        result = "bandwidth(" + properties[ 'trait_of' ][ 0 ].lower() + \
+            ", uplo())"
+
     #
     # The number of columns of op( A )
     #
@@ -1178,6 +1182,7 @@ def parse_file( filename, template_map ):
         #
         match_matrix_traits = re.compile( '(sub|super|rows|columns|order)([\-]?diagonals|with|in|of|the|band|input|\s)+(matrix|matrices|\s)+' + \
             '([A-Z]+\s+and\s+[A-Z]+|[A-Z]+)', re.M | re.S ).findall( comment_block )
+        match_banded_uplo = re.compile( '(number|of|sub|super|diagonals|if|UPLO)', re.M | re.S ).findall( comment_block )
         if len( match_matrix_traits ) == 1:
           print "Matched trait:", match_matrix_traits
           if match_matrix_traits[0][0] == 'order':
@@ -1210,7 +1215,10 @@ def parse_file( filename, template_map ):
               try_names = [ matrix_name.strip(), matrix_name.strip() + 'B' ]
               for try_name in try_names:
                 if try_name in grouped_arguments[ 'by_type' ][ 'matrix' ]:
-                  argument_properties[ 'trait_type' ] = 'num_' + match_matrix_traits[0][0]
+                  if len( match_banded_uplo ) == 0:
+                    argument_properties[ 'trait_type' ] = 'num_' + match_matrix_traits[0][0]
+                  else:
+                    argument_properties[ 'trait_type' ] = 'num_' + match_matrix_traits[0][0] + '_uplo'
                   argument_properties[ 'trait_of' ] = [ try_name.strip() ]
 
         #

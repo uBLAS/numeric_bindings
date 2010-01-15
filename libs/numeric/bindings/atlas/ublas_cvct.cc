91761e1b3358406c0b7ba9789f09955c87b35523
rutger <rutger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-15 15:11:56 +0000

    ublas_cvct now passes regression
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@59049 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/numeric/bindings/atlas/ublas_cvct.cc
+++ libs/numeric/bindings/atlas/ublas_cvct.cc
@@ -8,11 +8,11 @@
 //#define BOOST_NUMERIC_BINDINGS_POOR_MANS_TRAITS 
 //#define BOOST_NO_FUNCTION_TEMPLATE_ORDERING
 
-#include <boost/numeric/bindings/traits/ublas_vector.hpp>
-#include <boost/numeric/bindings/atlas/cblas1.hpp>
+#include <boost/numeric/bindings/ublas/vector.hpp>
+#include <boost/numeric/bindings/blas/level1.hpp>
 #include "utils.h" 
 
-namespace atlas = boost::numeric::bindings::atlas;
+namespace blas = boost::numeric::bindings::blas;
 namespace ublas = boost::numeric::ublas;
 
 using std::cout;
@@ -37,48 +37,48 @@ int main() {
   init_v (v, times_plus<cmplx_t> (cmplx_t (1, -1), cmplx_t (0, .1))); 
   print_v (v, "v"); 
 
-  atlas::scal (2.0, v); 
+  blas::scal (2.0, v); 
   print_v (v, "2.0 v"); 
 
-  atlas::scal (cmplx_t (-1, 0), v); 
+  blas::scal (cmplx_t (-1, 0), v); 
   print_v (v, "(-1, 0) v"); 
 
-  atlas::scal (cmplx_t (0, 1), v);
+  blas::scal (cmplx_t (0, 1), v);
   print_v (v, "(0, 1) v"); 
 
-  atlas::set (cmplx_t (1, -1), v); 
+  blas::set (cmplx_t (1, -1), v); 
   print_v (v, "v"); 
   vct_t v1 (n); 
-  atlas::set (cmplx_t (0, -1), v1); 
+  blas::set (cmplx_t (0, -1), v1); 
   print_v (v1, "v1"); 
 
   cout << endl; 
-  cout << "v^T v1 = " << atlas::dot (v, v1) << " == "
-    << atlas::dotu (v, v1) << " == "
+  cout << "v^T v1 = " << blas::dotc (v, v1) << " == "
+    << blas::dotu (v, v1) << " == "
     << inner_prod (v, v1) << endl; 
-  cout << "v^T v = " << atlas::dot (v, v) << " == "
-    << atlas::dotu (v, v) << " == "
+  cout << "v^T v = " << blas::dotc (v, v) << " == "
+    << blas::dotu (v, v) << " == "
     << inner_prod (v, v) << endl; 
-  cout << "v1^T v1 = " << atlas::dot (v1, v1) << " == "
-    << atlas::dotu (v1, v1) << " == "
+  cout << "v1^T v1 = " << blas::dotc (v1, v1) << " == "
+    << blas::dotu (v1, v1) << " == "
     << inner_prod (v1, v1) << endl; 
 
   cout << endl; 
-  cout << "v^H v1 = " << atlas::dotc (v, v1) << " == "
+  cout << "v^H v1 = " << blas::dotc (v, v1) << " == "
     << inner_prod (conj (v), v1) << " == "
     << inner_prod (v1, conj (v)) << " != "
     << inner_prod (v, conj (v1)) << endl; 
-  cout << "v^H v = " << atlas::dotc (v, v) << " == "
+  cout << "v^H v = " << blas::dotc (v, v) << " == "
     << inner_prod (conj (v), v) << " == "
     << inner_prod (v, conj (v)) << endl; 
-  cout << "v1^H v1 = " << atlas::dotc (v1, v1) << " == "
+  cout << "v1^H v1 = " << blas::dotc (v1, v1) << " == "
     << inner_prod (conj (v1), v1) << " == "
     << inner_prod (v1, conj (v1)) << endl; 
 
   
   cout << endl;
-  cout << "||v||_1 = " << atlas::asum (v) << endl; 
-  cout << "||v||_2 = " << atlas::nrm2 (v) << " == "
+  cout << "||v||_1 = " << blas::asum (v) << endl; 
+  cout << "||v||_2 = " << blas::nrm2 (v) << " == "
     << norm_2 (v) << endl; 
   
   cout << endl;

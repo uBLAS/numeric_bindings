0bfff52f9750acca87f0f99b0673af5152915d54
rutger <rutger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-15 14:16:43 +0000

    added bindings to ublas matrix slice, vector slice, matrix row
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@59041 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/numeric/bindings/ublas/vector_proxy.hpp
+++ boost/numeric/bindings/ublas/vector_proxy.hpp
@@ -11,6 +11,7 @@
 
 #include <boost/numeric/bindings/begin.hpp>
 #include <boost/numeric/bindings/detail/adaptor.hpp>
+#include <boost/numeric/bindings/detail/offset.hpp>
 #include <boost/numeric/bindings/detail/property_map.hpp>
 #include <boost/numeric/bindings/end.hpp>
 #include <boost/numeric/bindings/size.hpp>
@@ -38,6 +39,32 @@ struct adaptor< ublas::vector_range< T >, Id, Enable > {
 
     static typename result_of::end_value< adapted_type >::type end_value( Id& id ) {
         return bindings::begin_value( id.data() ) + id.size() * stride1( id );
+    }
+
+    static std::ptrdiff_t stride1( const Id& id ) {
+        return bindings::stride1( id.data() );
+    }
+
+};
+
+template< typename T, typename Id, typename Enable >
+struct adaptor< ublas::vector_slice< T >, Id, Enable > {
+
+    typedef typename copy_const< Id, T >::type adapted_type;
+    typedef typename property_map_of< adapted_type >::type property_map;
+
+    static std::ptrdiff_t size1( const Id& id ) {
+        return id.size();
+    }
+
+    static typename result_of::begin_value< adapted_type >::type begin_value( Id& id ) {
+        return bindings::begin_value( id.data() ) + 
+               offset( id, id.start() );
+    }
+
+    static typename result_of::end_value< adapted_type >::type end_value( Id& id ) {
+        return bindings::begin_value( id.data() ) + 
+               offset( id, id.start() + id.size() );
     }
 
     static std::ptrdiff_t stride1( const Id& id ) {

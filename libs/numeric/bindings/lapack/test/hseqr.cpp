0ec66edb515347d9420165878b044e0f9b8599ff
klimpel <klimpel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-15 01:14:32 +0000

    test for wrong assertions on output parameter -> test is now failing...
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@59015 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/numeric/bindings/lapack/test/hseqr.cpp
+++ libs/numeric/bindings/lapack/test/hseqr.cpp
@@ -102,7 +102,7 @@ void hseqr(int n){
     boost::numeric::bindings::detail::array<complex<double> > work_c(2*n);
     boost::numeric::bindings::detail::array<double> work_r(n);
     ublas::vector<bool> select_dummy(n);
-    integer_t m_info(0);
+    integer_t m_info(n+1);
     lapack::trevc('B','B',select_dummy,G,cVL,cVR,n,m_info,lapack::workspace(work_c,work_r));
 
     cout << "\n==================================" << endl;

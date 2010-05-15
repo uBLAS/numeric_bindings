0ca120532d4a7952d35f926ba79e60d63a479792
klimpel <klimpel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-05-15 23:49:48 +0000

    added compressed sparse matrix bindings for glas and mtl, and adjusted one of the tests to be able to test ublas/glas/mtl compressed sparse matrix bindings. Compressed sparse matrix bindings for eigen will follow soon (at least that's the plan).
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@62013 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/numeric/bindings/umfpack/test/umfpack_simple.cc
+++ libs/numeric/bindings/umfpack/test/umfpack_simple.cc
@@ -39,45 +39,77 @@
  *
  */
 
-/* Used by permission. */ 
+/* Used by permission. */
 
 
-/* Simple demo program for UMFPACK               */ 
+/* Simple demo program for UMFPACK               */
 /* from UMFPACK Version 4.1 Quick Start Guide    */
 
 /* modified by Kresimir Fresl, 2003              */
 /* UMFPACK bindings & ublas::compressed_matrix<> */
 
 
+#if !defined(TEST_MATLIB_UBLAS) && !defined(TEST_MATLIB_GLAS) && !defined(TEST_MATLIB_MTL) && !defined(TEST_MATLIB_EIGEN)
+#define TEST_MATLIB_UBLAS
+#endif
+
 #include <iostream>
-#include <boost/numeric/bindings/ublas/vector.hpp>
-#include <boost/numeric/bindings/ublas/matrix_sparse.hpp>
 #include <boost/numeric/bindings/umfpack/umfpack.hpp>
+#include <boost/numeric/bindings/ublas/vector.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
+#if defined(TEST_MATLIB_UBLAS)
+
+#include <boost/numeric/bindings/ublas/matrix_sparse.hpp>
 namespace ublas = boost::numeric::ublas;
+typedef ublas::compressed_matrix<double, ublas::column_major, 0, 
+    ublas::unbounded_array<int>, ublas::unbounded_array<double> > m_t;
+typedef m_t& mi_t;
+#define AA(i,j, val) a(i,j) = val
+
+#elif defined(TEST_MATLIB_GLAS)
+
+#include <boost/numeric/bindings/glas/compressed.hpp>
+typedef glas::sparse_matrix< double, glas::compressed_sparse_structure<
+    glas::column_orientation, std::ptrdiff_t, std::ptrdiff_t, 0> > m_t;
+typedef m_t& mi_t;
+#define AA(i,j, val) insert(a,i,j, val)
+
+#elif defined(TEST_MATLIB_MTL)
+
+#include <boost/numeric/bindings/mtl/compressed2D.hpp>
+typedef mtl::compressed2D<double, mtl::matrix::parameters<mtl::tag::col_major> > m_t;
+typedef mtl::matrix::inserter<m_t> mi_t;
+#define AA(i,j, val) a(i,j) = val
+
+#elif defined(TEST_MATLIB_EIGEN)
+
+#endif
+
+typedef boost::numeric::ublas::vector<double> v_t;
 namespace umf = boost::numeric::bindings::umfpack;
 
 int main() {
 
-  ublas::compressed_matrix<double, ublas::column_major, 0, 
-    ublas::unbounded_array<int>, ublas::unbounded_array<double> > A (5,5,12); 
-  ublas::vector<double> B (5), X (5);
+  m_t A (5,5,12);
+  v_t B (5), X (5);
 
-  A(0,0) = 2.; A(0,1) = 3; 
-  A(1,0) = 3.; A(1,2) = 4.; A(1,4) = 6;
-  A(2,1) = -1.; A(2,2) = -3.; A(2,3) = 2.;
-  A(3,2) = 1.;
-  A(4,1) = 4.; A(4,2) = 2.; A(4,4) = 1.; 
-
-  B(0) = 8.; B(1) = 45.; B(2) = -3.; B(3) = 3.; B(4) = 19.; 
+  {
+    mi_t a(A);
+    AA(0,0, 2.); AA(0,1, 3.);
+    AA(1,0, 3.); AA(1,2, 4.); AA(1,4, 6.);
+    AA(2,1,-1.); AA(2,2,-3.); AA(2,3, 2.);
+    AA(3,2, 1.);
+    AA(4,1, 4.); AA(4,2, 2.); AA(4,4, 1.);
+  }
+  B(0) = 8.; B(1) = 45.; B(2) = -3.; B(3) = 3.; B(4) = 19.;
 
   umf::symbolic_type<double> Symbolic;
   umf::numeric_type<double> Numeric;
 
-  umf::symbolic (A, Symbolic); 
-  umf::numeric (A, Symbolic, Numeric); 
-  umf::solve (A, X, B, Numeric);   
+  umf::symbolic (A, Symbolic);
+  umf::numeric (A, Symbolic, Numeric);
+  umf::solve (A, X, B, Numeric);
 
   std::cout << X << std::endl;  // output: [5](1,2,3,4,5)
-} 
+}

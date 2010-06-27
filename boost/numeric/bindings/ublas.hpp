f9f6698c3f0c5c00ae823d14123e7edf4e5c59da
klimpel <klimpel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-06-27 12:10:25 +0000

    Updated ublas.hpp
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@63381 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/numeric/bindings/ublas.hpp
+++ boost/numeric/bindings/ublas.hpp
@@ -9,16 +9,19 @@
 #ifndef BOOST_NUMERIC_BINDINGS_UBLAS_HPP
 #define BOOST_NUMERIC_BINDINGS_UBLAS_HPP
 
+#include <boost/numeric/bindings/ublas/banded.hpp>
 #include <boost/numeric/bindings/ublas/hermitian.hpp>
 #include <boost/numeric/bindings/ublas/matrix.hpp>
 #include <boost/numeric/bindings/ublas/matrix_expression.hpp>
 #include <boost/numeric/bindings/ublas/matrix_proxy.hpp>
 #include <boost/numeric/bindings/ublas/matrix_sparse.hpp>
+#include <boost/numeric/bindings/ublas/scalar_vector.hpp>
+#include <boost/numeric/bindings/ublas/storage.hpp>
 #include <boost/numeric/bindings/ublas/symmetric.hpp>
 #include <boost/numeric/bindings/ublas/triangular.hpp>
 #include <boost/numeric/bindings/ublas/vector.hpp>
-#include <boost/numeric/bindings/ublas/vector_sparse.hpp>
-#include <boost/numeric/bindings/ublas/vector_proxy.hpp>
 #include <boost/numeric/bindings/ublas/vector_expression.hpp>
+#include <boost/numeric/bindings/ublas/vector_proxy.hpp>
+#include <boost/numeric/bindings/ublas/vector_sparse.hpp>
 
 #endif

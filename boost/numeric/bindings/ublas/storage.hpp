5dd58f060f944d615cc97c78158d41da3a143d23
rutger <rutger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-03-01 08:22:45 +0000

    Applied path to ublas/storage from Andrey Asadchev
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/numeric_bindings@69434 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/numeric/bindings/ublas/storage.hpp
+++ boost/numeric/bindings/ublas/storage.hpp
@@ -1,5 +1,6 @@
 //
 // Copyright (c) 2009 Rutger ter Borg
+// Copyright (c) 2011 Andrey Asadchev
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
@@ -11,42 +12,17 @@
 
 #include <boost/numeric/bindings/detail/adaptor.hpp>
 #include <boost/numeric/ublas/storage.hpp>
+#include <boost/type_traits/is_base_of.hpp>
 
 namespace boost {
 namespace numeric {
 namespace bindings {
 namespace detail {
 
-template< typename T, std::size_t N, typename Alloc, typename Id, typename Enable >
-struct adaptor< ::boost::numeric::ublas::bounded_array< T, N, Alloc >, Id, Enable > {
+template< typename T, typename Id >
+struct adaptor< T, Id, typename boost::enable_if< boost::is_base_of< ublas::storage_array<T>, T> >::type > {
 
-    typedef typename copy_const< Id, T >::type value_type;
-    typedef mpl::map<
-        mpl::pair< tag::value_type, value_type >,
-        mpl::pair< tag::entity, tag::vector >,
-        mpl::pair< tag::size_type<1>, std::ptrdiff_t >,
-        mpl::pair< tag::data_structure, tag::linear_array >,
-        mpl::pair< tag::stride_type<1>, tag::contiguous >
-    > property_map;
-
-    static std::ptrdiff_t size1( const Id& id ) {
-        return id.size();
-    }
-
-    static value_type* begin_value( Id& id ) {
-        return id.begin();
-    }
-
-    static value_type* end_value( Id& id ) {
-        return id.end();
-    }
-
-};
-
-template< typename T, typename Alloc, typename Id, typename Enable >
-struct adaptor< ublas::unbounded_array< T, Alloc >, Id, Enable > {
-
-    typedef typename copy_const< Id, T >::type value_type;
+    typedef typename copy_const< Id, typename T::value_type >::type value_type;
     typedef mpl::map<
         mpl::pair< tag::value_type, value_type >,
         mpl::pair< tag::entity, tag::vector >,
